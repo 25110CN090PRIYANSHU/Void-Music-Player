3da3cbbcedd4742079320b733a2a0c4cdd6b5dd9
25110CN090PRIYANSHU <priyanshusingh221100@gmail.com>
2026-08-23 02:52:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get("/api/search",async(req,res)=>{
     }
 });
 
-app.listen(PORT,()=>{
+app.listen(PORT,"0.0.0.0",()=>{
     console.log("\n================================");
     console.log("       VOID MUSIC PLAYER");
     console.log("================================");
